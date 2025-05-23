abbdf32b60ec405d53bb4cc87384c6fe4b593b73
TutiPY <matuteramalho@gmail.com>
2025-05-23 16:55:16 -0300

    Un flawn

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
 basic.forever(function () {
-	
+    basic.showArrow(ArrowNames.North)
+    basic.pause(200)
+    if (input.isGesture(Gesture.LogoUp)) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(200)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.pause(200)
+    }
+    basic.showArrow(ArrowNames.South)
+    basic.pause(200)
+    if (input.isGesture(Gesture.LogoDown)) {
+        basic.showLeds(`
+            . . . # .
+            . . # . #
+            . # . . .
+            # . . . .
+            . . . . .
+            `)
+        basic.pause(200)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.pause(200)
+    }
 })
