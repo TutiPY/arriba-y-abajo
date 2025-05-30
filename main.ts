6009dcdd09ddcd0afc4f74d478ae3d975ef70529
TutiPY <matuteramalho@gmail.com>
2025-05-30 16:19:17 -0300

    Tuti

--- main.ts
+++ main.ts
@@ -23,4 +23,22 @@ basic.forever(function () {
         basic.showIcon(IconNames.No)
         basic.pause(200)
     }
+    basic.showArrow(ArrowNames.West)
+    basic.pause(200)
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(200)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.pause(200)
+    }
+    basic.showArrow(ArrowNames.East)
+    basic.pause(200)
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(200)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.pause(200)
+    }
 })
